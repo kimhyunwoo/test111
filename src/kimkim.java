ffe19ebfbb7a11798f829b7d8219d6a79dabac1a
User <gus1891@hanmail.net>
2014-06-09 21:34:18 +0900

    java변경함 후두두두두

--- src/kimkim.java
+++ src/kimkim.java
@@ -3,6 +3,7 @@ public class kimkim {
 public static void main(String[] args)
 {
 	System.out.println("¾È³çÇÏ¼¼¿è");
+	System.out.println("¿¹½º¸Ç ¿ä ÆÄ·òÅõ³ªÀÕ");
 }
 }
 
---
+++ src/seeeee.java
@@ -0,0 +1,8 @@
+
+public class seeeee {
+	public static void main(String[] args)
+	{
+		System.out.println("ddddd");
+	}
+
+}
